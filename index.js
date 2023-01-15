895dcba2d9dc4d8ad3d884ebe4941692b434247f
Nick Johnson <nlj222@gmail.com>
2023-01-14 19:18:07 -0800

    lab complete

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+    console.log(json);
+    renderBooks(json);
+    findFifthBook(json);
+    find1031stChar(json);
+    findTotalPages(json);
+  })
 }
+
+// function fetchCharacters() {
+//   // To pass the tests, don't forget to return your fetch!
+//   return fetch("https://anapioficeandfire.com/api/characters")
+//   .then((resp) => resp.json())
+//   .then((json) => {
+//     console.log(json);
+//     find1031stChar(json);
+//   })
+// }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -14,4 +32,17 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+  fetchCharacters();
 });
+
+function findFifthBook(books) {
+  console.log(books[4].name);
+}
+
+function find1031stChar(books){
+  console.log(books[4].characters[1031]);
+}
+
+function findTotalPages(books){
+
+}
